0bcad9c9689598226b70d9f2d03d40a7bb099dad
gh0st <ghost_shellc@proton.me>
2026-01-24 17:14:55 -0500

    feat: agregado estructura backend postgresql prisma

---
+++ server/prisma/seed.ts
@@ -0,0 +1,94 @@
+//import {PrismaClient} from "../generated/prisma/client.ts";
+import { PrismaClient } from "../generated/prisma/client.js";
+import {PrismaPg} from "@prisma/adapter-pg"
+import dotenv from "dotenv"
+import {Pool} from "pg";
+import fs from "fs"
+
+import path from "path";
+import {dirname} from "path"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dataDirectory = path.join(__dirname, "seedData");
+
+dotenv.config()
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
+
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({adapter})
+
+async function deleteAllData(orderedFileNames: string[]) {
+  const modelNames = orderedFileNames.map((fileName) => {
+    const modelName = path.basename(fileName, path.extname(fileName));
+    return modelName.charAt(0).toUpperCase() + modelName.slice(1);
+  });
+
+  for (const modelName of modelNames) {
+    const model: any = prisma[modelName as keyof typeof prisma];
+    if (model) {
+      await model.deleteMany({});
+      console.log(`Cleared data from ${modelName}`);
+    } else {
+      console.error(
+        `Model ${modelName} not found. Please ensure the model name is correctly specified.`
+      );
+    }
+  }
+}
+
+async function main() {
+  const dataDirectory = path.join(__dirname, "seedData");
+
+  const orderedFileNames = [
+    "products.json",
+    "expenseSummary.json",
+    "sales.json",
+    "salesSummary.json",
+    "purchases.json",
+    "purchaseSummary.json",
+    "users.json",
+    "expenses.json",
+    "expenseByCategory.json",
+  ];
+
+  await deleteAllData(orderedFileNames);
+
+  for (const fileName of orderedFileNames) {
+    const filePath = path.join(dataDirectory, fileName);
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const modelName = path.basename(fileName, path.extname(fileName));
+    const model: any = prisma[modelName as keyof typeof prisma];
+
+    if (!model) {
+      console.error(`No Prisma model matches the file name: ${fileName}`);
+      continue;
+    }
+
+    for (const data of jsonData) {
+      await model.create({
+        data,
+      });
+    }
+
+    console.log(`Seeded ${modelName} with data from ${fileName}`);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+
