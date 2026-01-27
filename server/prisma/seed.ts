8ffc923d68e389745add9281440e2ae331817703
gh0st <ghost_shellc@proton.me>
2026-01-27 17:26:19 -0500

    Error de commonjs esmodule prisma postgresql

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,5 +1,5 @@
-//import {PrismaClient} from "../generated/prisma/client.ts";
-import { PrismaClient } from "../generated/prisma/client.js";
+import {PrismaClient} from "@prisma/client";
+//import { PrismaClient } from "../generated/prisma/client.js";
 import {PrismaPg} from "@prisma/adapter-pg"
 import dotenv from "dotenv"
 import {Pool} from "pg";
--- server/src/controllers/dashboardController.ts
+++ server/src/controllers/dashboardController.ts
@@ -1,7 +1,21 @@
-import type {Request, Response} from "express"
-import { PrismaClient } from "@prisma/client/extension"
+import type { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-const prisma = new PrismaClient()
+dotenv.config();
+
+// Configura el pool igual que en seed.ts
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });  // ← PASA EL ADAPTER
 
 export const getDashboardMetrics = async(req: Request, res: Response):Promise<void> => {
     try {
@@ -29,13 +43,13 @@ export const getDashboardMetrics = async(req: Request, res: Response):Promise<vo
                 date: "desc",
             }
         })
-        const expenseByCategorySummaryRaw = await prisma.expenseByCategorySummaryRaw.findMany({
+        const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany({
             take: 5,
             orderBy: {
                 date: "desc",
             }
         })
-        const expenseByCategory = expenseByCategorySummaryRaw.map(
+        const expenseByCategorySummary = expenseByCategorySummaryRaw.map(
             (item: any) => ({
                 ...item,
                 ammount: item.amount.toString()
@@ -44,8 +58,9 @@ export const getDashboardMetrics = async(req: Request, res: Response):Promise<vo
         res.json({
             popularProducts,
             salesSummary,
+            purchaseSummary,
             expenseSummary,
-            expenseByCategory
+            expenseByCategorySummary,
         })
     } catch (error) {
         res.status(500).json({message: "Error retrieving dashboard metrics"})
